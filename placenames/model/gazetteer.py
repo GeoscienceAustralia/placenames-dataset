66a5aace24c58ae2de84e6af97064fedb6f305cf
bellie888 <joseph.bell@ga.gov.au>
2019-08-21 12:23:13 +1000

    Added real links to hardcoded gazetteer websites in previous push Added naming authorities, their website and email into hard coding this information will be hosted elsewhere one day

--- placenames/model/gazetteer.py
+++ placenames/model/gazetteer.py
@@ -16,7 +16,7 @@ GAZETTEERS = {
     'AAD': {
         'label': 'Australian Antarctic Place Names Gazetteer',
         'uri_id': 'https://data.aad.gov.au/aadc/gaz/'
-    },
+            },
     'ACT': {
         'label': 'Australian Capital Territory Place Names Gazetteer',
         'uri_id': 'http://app.actmapi.act.gov.au/actmapi/index.html?viewer=pn'
@@ -54,6 +54,59 @@ GAZETTEERS = {
         'uri_id': 'https://www0.landgate.wa.gov.au/maps-and-imagery/wa-geographic-names'
     }
 }
+NAME_AUTHORITIES = {
+    'AAD': {
+        'label': 'Australian Antarctic Place Names Gazetteer',
+        'web': 'http://data.aad.gov.au',
+        'email':'placenames@antarctica.gov.au'
+            },
+    'ACT': {
+        'label': 'ACT Environment, Planning and Sustainable Development Directorate',
+        'web': 'https://www.planning.act.gov.au/tools_resources/place_names',
+        'email':'placenames@act.gov.au  '
+    },
+    'AHO': {
+        'label': 'Australian Hydrographic Office',
+        'web': 'http://www.hydro.gov.au/',
+        'email':'datacentre@hydro.gov.au'
+    },
+    'NSW': {
+        'label': 'NSW Spatial Services',
+        'web': 'http://www.gnb.nsw.gov.au',
+        'email': 'Ss-gnb@finance.nsw.gov.au'
+    },
+    'NT': {
+        'label': 'NT Dept of Infrastructure, Planning and Logistics',
+        'web': 'https://www.ntlis.nt.gov.au/placenames/',
+        'email':'place.names@nt.gov.au'
+    },
+    'QLD': {
+        'label': 'QLD Dept of Natural Resources, Mines and Energy ',
+        'web': 'https://www.dnrm.qld.gov.au/qld/environment/land/place-names/search',
+        'email':'qldplacenames@dnrme.qld.gov.au'
+    },
+    'SA': {
+        'label': 'SA Dept for Transport, Energy & Infrastructure',
+        'web': 'www.sa.gov.au/landservices/namingproposals',
+        'email':'LSGPlaceNames@sa.gov.au'
+    },
+    'TAS': {
+        'label': 'Tasmanian Dept of Primary Industries, Parks, Water and Environment',
+        'web': 'www.placenames.tas.gov.au',
+        'email':'Nomenclature.Office@dpipwe.tas.gov.au'
+    },
+    'VIC': {
+        'label': 'Victorian Dept of Environment, Land, Water & Planning',
+        'web': 'https://www.propertyandlandtitles.vic.gov.au/naming-places-features-and-roads/naming-rules-for-places-in-victoria',
+        'email':'geo.names@delwp.vic.gov.au'
+    },
+    'WA': {
+        'label': 'WA Landgate',
+        'web': 'http://www.landgate.wa.gov.au',
+        'email':'geographicnames@landgate.wa.gov.au  '
+    }
+}
+
 
 DATE_MODIFIED = '2019-06-06 12:18:00.525998'
 
