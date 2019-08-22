eb3b0496021b75150f1473f89ed3ea20eea54ece
bellie888 <joseph.bell@ga.gov.au>
2019-08-22 13:41:03 +1000

    Fixed imports

--- placenames/model/placename.py
+++ placenames/model/placename.py
@@ -12,7 +12,7 @@ from rdflib import Graph, URIRef, RDF, XSD, Namespace, Literal
 from .gazetteer import GAZETTEERS, NAME_AUTHORITIES
 
 # for DGGS zone attribution
-import dggs
+from AusPIXengine import dggs
 rdggs = dggs.RHEALPixDGGS()
 
 
